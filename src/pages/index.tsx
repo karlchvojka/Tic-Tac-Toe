97b1f8462d2705988df809cb718e6f399d829123
karlchvojka <karl.chvojka@gmail.com>
2025-06-12 19:44:36 -0400

    FIX: Adding Starting point from docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
+function Square() {
+  return <button className={styles.square}>X</button>
+}
+
 export default function Home() {
   return (
     <>
@@ -10,11 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page}`}
-      >
+      <div className={`${styles.page}`} >
         <main className={styles.main}>
-          <p>Main</p>
+          <Square />
         </main>
       </div>
     </>
