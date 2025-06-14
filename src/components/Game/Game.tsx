550211ba6bd6b0cc11a64c45411ef5c537731320
karlchvojka <karl.chvojka@gmail.com>
2025-06-13 21:37:02 -0400

    FEAT: Implement Game move history

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useState } from "react";
-import Board from '../Board/Board'
+import Board from '../Board/Board';
+
+import styles from "./Game.module.css";
 
 /**
  * Game Component
@@ -14,10 +16,10 @@ export default function Game() {
    * xIsNext tracks which turn it is (X or O)
    * history tracks each move throughout a game.
    */
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState<string[][]>([Array(9).fill(null)])
-
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   /**
    * Handle Play
@@ -27,17 +29,37 @@ export default function Game() {
    * @param nextSquares 
    */
   function handlePlay(nextSquares: Array<string>) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
-  } 
+    const nextHistory = [...history.slice(0, currentMove +1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
   return (
-    <div className="game">
+    <div className={styles.game}>
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div className="game-info">
-        <ol><li><p>test</p></li></ol>
+      <div className={styles.gameInfo}>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
